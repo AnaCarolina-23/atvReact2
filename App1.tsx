6c66db7a19bf1f2574d08c48164077e862ec75dd
Ana Carolina :) <anacarolina.ribeiro.dev2024@gmail.com>
2025-04-03 15:54:57 -0300

    Add files via upload

---
+++ App1.tsx
@@ -0,0 +1,18 @@
+import Header from "./Header"
+import Content from "./Content"
+import Footer from "./Footer"
+import './styles1.css'
+
+function App() {
+  
+
+  return (
+    <div className="container">
+      <Header />
+      <Content />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
